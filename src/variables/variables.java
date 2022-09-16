b298a3bf1f304dbce8f33bcd62758b65141ab6e6
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-16 23:09:39 +0300

    Create variables.java

---
+++ src/variables/variables.java
@@ -0,0 +1,12 @@
+package variables;
+    // camel casing = basHarfleriBuyukYazmak
+public class variables {
+    public static void main(String[] args) {
+        // case sensitive language = büyük küçük harf duyarlı dil
+        int studentsNumber = 11;
+        String message = "Öğrenci Sayısı : ";
+        System.out.println(message + studentsNumber); 
+
+
+    }
+}
