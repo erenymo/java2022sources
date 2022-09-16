21ed19c2d20896b7c0fd5083027bdbf0ee729746
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-16 23:08:59 +0300

    Create hello.java

---
+++ src/javahelloworld/hello.java
@@ -0,0 +1,7 @@
+package javahelloworld;
+public class hello {
+    public static void main(String[] args) {  
+        System.out.println("hello java");
+        System.out.println("Merhaba Java 2");
+    }
+}
