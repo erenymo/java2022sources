d73f1865e254cb2e16ccdbdbd2692c6589db7493
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-16 23:10:47 +0300

    Create conditionals.java

---
+++ src/conditionals/conditionals.java
@@ -0,0 +1,14 @@
+package conditionals;
+
+public class conditionals {
+    public static void main(String[] args) {
+        int sayi = 34;
+        if(sayi<20){
+            System.out.println("Girdiğiniz sayı 20den küçüktür");
+        }else if(sayi==20){
+            System.out.println("Girdiğiniz sayı 20ye eşittir");
+        }else{
+            System.out.println("Girdiğiniz sayı 20den büyüktür");
+        }
+    }
+}
