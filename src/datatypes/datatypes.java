ef79852abed0a51112d856b2805f3fff44095c14
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-16 23:10:59 +0300

    Create datatypes.java

---
+++ src/datatypes/datatypes.java
@@ -0,0 +1,21 @@
+package datatypes;
+
+public class datatypes {
+    public static void main(String[] args) {
+       int sayi = 12;
+       System.out.println(sayi);
+
+       sayi = 13;
+       System.out.println(sayi);
+
+       double number = 2.5;
+
+       char karakter = 'A';  // Tek tırnak tek harf tutar.
+
+       boolean durum = true; // boolean = true or false değerleri tutar.
+
+        System.out.println(number+" "+karakter+" "+durum);
+    
+
+    }
+}
