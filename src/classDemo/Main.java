e001bd629ff6b87ca04fb6cee979085878f9f166
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-22 13:09:07 +0300

    Create Main.java

---
+++ src/classDemo/Main.java
@@ -0,0 +1,27 @@
+package classDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        // Classlar referans tipleridir.
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.update();
+        customerManager.add();
+        customerManager.delete();
+
+        //value tip
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+
+        System.out.println(sayi2);
+
+        //referans tiptir
+        int[] sayilar1 = new int[]{1,2,3};
+        int[] sayilar2 = new int[]{4,5,6};
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
+    }
+}
+
