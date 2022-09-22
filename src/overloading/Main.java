5b979c34d1b2432db10f4e631c6a552c875f5f2f
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-22 13:11:26 +0300

    Create Main.java

---
+++ src/overloading/Main.java
@@ -0,0 +1,10 @@
+package overloading;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+
+        System.out.println(dortIslem.topla(2, 3));
+        System.out.println(dortIslem.topla(3,5,6));
+    }
+}
