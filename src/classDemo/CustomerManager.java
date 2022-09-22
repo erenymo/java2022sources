5b229ea7db69852220374468041d6d12a27571dd
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-22 13:08:58 +0300

    Create CustomerManager.java

---
+++ src/classDemo/CustomerManager.java
@@ -0,0 +1,15 @@
+package classDemo;
+
+public class CustomerManager {
+    public void add(){
+        System.out.println("Üye sisteme başarıyla eklendi.");
+    }
+
+    public void delete(){
+        System.out.println("Üye sistemden başarıyla silindi.");
+    }
+    
+    public void update(){
+        System.out.println("Üye başarıyla güncellendi.");
+    }
+}
