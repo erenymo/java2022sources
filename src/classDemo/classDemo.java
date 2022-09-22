60fd6467a0116179cabbbc92f25f2d4d9e956091
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-22 13:08:48 +0300

    Create classDemo.java

---
+++ src/classDemo/classDemo.java
@@ -0,0 +1,10 @@
+package classDemo;
+
+public class classDemo {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+
+        int sonuc = dortIslem.Topla(3, 4);
+        System.out.println(sonuc);
+    }
+}
