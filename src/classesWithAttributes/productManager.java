ad4896603f330936577dfb57286f0ad28a0515bf
erenymo <87265677+erenymo@users.noreply.github.com>
2022-09-22 13:10:14 +0300

    Create productManager.java

---
+++ src/classesWithAttributes/productManager.java
@@ -0,0 +1,14 @@
+package classesWithAttributes;
+
+public class productManager {
+    public void add(product product){
+        //JDBC
+        System.out.println("Ürün Eklendi " + product.getName());
+    }
+
+    public void add2(int id, String name, String description, int stockAmount, double price, String color){
+
+
+    }
+}
+
